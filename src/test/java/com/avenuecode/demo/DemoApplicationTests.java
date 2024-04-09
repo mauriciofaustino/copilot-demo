7a607970655041cff03ef1faf417a75bb78b9a3a
Mauricio Castro <maucast@gap.com>
2024-04-09 20:20:41 -0300

    [@mauriciofaustino] adds config

--- src/test/java/com/avenuecode/demo/DemoApplicationTests.java
+++ src/test/java/com/avenuecode/demo/DemoApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class DemoApplicationTests {
